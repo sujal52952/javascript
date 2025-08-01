39ad556b4a73d845aa176d04adf02cc943d01dc3
sujal52952 <sujal1412.be22@chitkarauniversity.edu.in>
2025-08-01 19:49:39 +0000

    container.config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=564
+let accountEmail="sujal52952@gmail.com"
+var accountPassword="654"
+accountCity="kangra"
+
+
+accountEmail="grs@gamil.com"
+accountPassword="654485"
+accountCity="Dharamshala"
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
