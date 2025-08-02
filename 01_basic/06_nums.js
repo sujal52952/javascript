a4a2a0c01dbc029c36dd3aa8379a790c8831ac60
sujal52952 <sujal1412.be22@chitkarauniversity.edu.in>
2025-08-02 16:06:58 +0000

    done

---
+++ 01_basic/06_nums.js
@@ -0,0 +1,24 @@
+// const otherNumber =143.896548
+// console.log(otherNumber.toPrecision(3));
+
+
+// console.log(otherNumber.toFixed(3));
+
+
+// const hundreds=1000000000
+// console.log(hundreds.toLocaleString('en-IN'));//en means indian currency
+
+// console.log(Math);
+
+// console.log(Math.abs(-4)); // abs means absolute which change -ve values to +ve;
+
+// console.log(Math.round(4.6)); //round means roundoff which give roundoff values;
+
+
+// console.log(Math.ceil(4.1));//ceil means if the value is little bit bigger than 4 than it gives roundoff of next no. here it give 5
+
+// console.log(Math.floor(4.9));//floor is opposite to ceil it always give lower value here it give 4
+// console.log(Math.floor(2.54));
+
+console.log(Math.random());
+
