c268de5f2ebb33e4e38f491edc46672b8721a89e
sujal52952 <sujal1412.be22@chitkarauniversity.edu.in>
2025-08-02 16:29:39 +0000

    done

---
+++ 01_basic/07_datesinjs.js
@@ -0,0 +1,15 @@
+let myDate=new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());// Main
+// console.log(myDate.toDateString());// Main
+
+// let mycreatedate=new Date(2025,8,5) ///moths in javascript starts from 0 to 11
+// console.log(mycreatedate.toDateString());
+
+
+let mytimestamp=Date.now()
+console.log(mytimestamp);
