ee406306d5f9eeb2b022cfc37f9550c490848b21
sujal52952 <sujal1412.be22@chitkarauniversity.edu.in>
2025-08-03 17:37:49 +0000

    done

---
+++ 02_basic/04_objects2.js
@@ -0,0 +1,14 @@
+const course ={
+    coursename:"js in hindi",
+    price:"999",
+    courseinstructor:"sujal",
+}
+
+    const{courseinstructor:instructor}=course
+
+console.log(instructor);
+
+
+const navbar=()=>{
+    
+}
