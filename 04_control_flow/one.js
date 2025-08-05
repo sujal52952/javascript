71a7f81f88ea9b46eb196a8c944a65ad8da1f599
sujal52952 <sujal1412.be22@chitkarauniversity.edu.in>
2025-08-05 06:10:22 +0000

    donee

---
+++ 04_control_flow/one.js
@@ -0,0 +1,50 @@
+//if statement
+
+
+
+//VAR is a global scope
+
+
+
+// const month=3
+// switch(month){
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("February")
+//         break;
+//     case 3:
+//         console.log("March")
+//         break;
+//     case 4:
+//         console.log("April")
+//         break;
+//         default:
+//             console.log("not match")
+//             break;
+// }
+
+
+
+
+// const username=" "
+// if(username){
+//     console.log("got user email")
+// }
+// else{
+//     console.log("dont have user email")
+// }
+
+
+// //flase value-
+
+// false,0,-0,null,"",undefined,NaN     ----------check these values on username
+
+
+//truthy values
+
+//"0",'false," ",[],{},function(){}
+
+
+
